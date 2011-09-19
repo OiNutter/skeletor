1c2db65e860ebf0ffcac8fdcb4c620cbf7369789
OiNutter <willmckenzie@oinutter.co.uk>
2011-09-19 23:55:22 +0100

    Added Command Line Interface
    Added command line interface using Thor.  Added thor dependency to gem
    spec. Created build and clean tasks.

---
+++ lib/skeletor/cli.rb
@@ -0,0 +1,33 @@
+require 'thor'
+
+module Skeletor
+  
+  class CLI < Thor
+    
+    desc "build TEMPLATE", "Build project skeleton from TEMPLATE"
+    method_option :directory,             
+                  :aliases => "-d",
+                  :desc => "Sets the target directory. Defaults to current directory."
+    method_option :project, 
+                  :aliases => "-p",
+                  :desc => "Sets the project name. Defaults to current directory name."
+    def build(template)
+      path = options[:directory] || Dir.pwd
+      project = options[:project] || File.basename(path)
+      
+      skeleton = Builder.new(project,template,path)
+      skeleton.build
+    end
+
+    desc "clean" ,"clean directory"
+    method_option :directory,
+                  :aliases => "-d",
+                  :desc => "Sets the target directory. Defaults to current directory"
+    def clean
+      path = options[:directory] || Dir.pwd   
+      Builder.clean path
+    end
+    
+  end
+  
+end
--- skeletor.gemspec
+++ skeletor.gemspec
@@ -9,11 +9,13 @@ Gem::Specification.new do |s|
   s.summary = "Gem for creating project skeletons based on YAML templates"
   s.description = "Skeletor is a Ruby Gem for creating skeleton directory structures based on a YAML template."
 
-  s.files = Dir["README.md", "LICENSE", "lib/**/*.rb"]
-
+  s.files = Dir["README.md", "LICENSE", "lib/**/*.rb","bin/**/*","lib/skeletor/templates/**/*"]
+  s.executables = ["skeletor"]
   s.authors = ["Will McKenzie"]
   s.email = ["will@oinutter.co.uk"]
   s.homepage = "http://github.com/OiNutter/skeletor"
   s.rubyforge_project = "skeletor"
+  
+  s.add_dependency "thor"
 end
 
