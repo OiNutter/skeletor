9cde408223eabaf5f1106924a0592d48c391b68f
OiNutter <willmckenzie@oinutter.co.uk>
2011-09-25 08:51:23 +0100

    Added validate test to test script

--- test/test.rb
+++ test/test.rb
@@ -1,9 +1,12 @@
-require_relative '../lib/skeletor/templates'
+require_relative '../lib/skeletor/skeletons/loader'
+require_relative '../lib/skeletor/skeletons/validator'
+require_relative '../lib/skeletor/skeletons/skeleton'
+
 require_relative '../lib/skeletor/builder'
 
-path = File.expand_path(File.join(File.dirname(__FILE__), "test_project"))
+path = File.expand_path(File.join(File.dirname(__FILE__), "test-template"))
 
-skeleton = Skeletor::Builder.new('test_project','js-lib',path)
+#skeleton = Skeletor::Builder.new('test_project','js-lib',path)
 #skeleton = Skeletor::Templates::Skeleton.new('js-lib')
 #skeleton = YAML.load_file(File.join(Skeletor::Templates::Loader::TEMPLATE_PATH,'js-lib/js-lib.yml'))
 
@@ -13,6 +16,6 @@ skeleton = Skeletor::Builder.new('test_project','js-lib',path)
 #  
 #  puts dir
 #}
-skeleton.clean()
-skeleton.build()
-
+skeleton = Skeletor::Skeletons::Loader.loadTemplate(path)
+validator = Skeletor::Skeletons::Validator.new(skeleton)
+validator.validate()
